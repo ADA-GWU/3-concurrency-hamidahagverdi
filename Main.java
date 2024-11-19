141bf4954354136ea6891d028559e1953ed2f43a
hamida_hagverdiyeva <hamida.hagverdi1@gmail.com>
2024-11-19 12:30:09 +0400

    Initial setup: Basic image loading and display functionality - Added command line argument handling - Implemented basic image loading - Created simple GUI window for image display

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("current");
+    public static void main(String[] args) throws IOException {
+        if (args.length < 3) {
+            System.out.println("Usage: java Main <image-file> <square-size> <mode>");
+            return;
+        }
+
+        final String filename = args[0];
+        final int squareSize = Integer.parseInt(args[1]);
+        final String mode = args[2].toUpperCase();
+
+        final BufferedImage originalImage = ImageIO.read(new File(filename));
+
+        // Basic display
+        JFrame frame = new JFrame("Image Pixelator");
+        final JLabel label = new JLabel(new ImageIcon(originalImage));
+        frame.add(label);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
