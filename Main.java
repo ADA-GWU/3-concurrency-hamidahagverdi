160c41e3917f7b5a5d7010ef1b2e823f9d17e680
hamida_hagverdiyeva <hamida.hagverdi1@gmail.com>
2024-11-19 09:56:20 +0400

    asdasdasd

--- Main.java
+++ Main.java
@@ -1,12 +1,3 @@
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.awt.image.ColorModel;
-import java.awt.image.WritableRaster;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
