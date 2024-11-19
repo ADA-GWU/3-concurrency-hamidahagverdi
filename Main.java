88d47acb76c7da663f83d5d80f48674fd6951fd6
hamida_hagverdiyeva <hamida.hagverdi1@gmail.com>
2024-11-19 09:53:29 +0400

    asdasdasd

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
     }
 }
