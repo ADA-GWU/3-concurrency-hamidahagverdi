ef556a99ad9b11ee6305824d20f83b991c5ce234
hamida_hagverdiyeva <hamida.hagverdi1@gmail.com>
2024-11-20 20:12:24 +0400

    Added screen size check and resizing functionality

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 package src;
-
 import src.utils.ImageUtils;
 import src.utils.ThreadUtils;
 import src.utils.LoggerUtils;
-
 import src.models.ImageProcessor;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.logging.Level;
 
@@ -23,12 +22,31 @@ public class Main {
             int squareSize = Integer.parseInt(args[1]);
             String mode = args[2].toUpperCase();
 
+            // Check screen size and resize if needed
+            Dimension displaySize = Toolkit.getDefaultToolkit().getScreenSize();
+            if (image.getWidth() > displaySize.width || image.getHeight() > displaySize.height) {
+                double ratio = Math.min(
+                        (double) displaySize.width / image.getWidth(),
+                        (double) displaySize.height / image.getHeight()
+                );
+                int scaledWidth = (int) (image.getWidth() * ratio);
+                int scaledHeight = (int) (image.getHeight() * ratio);
+
+                BufferedImage scaledImage = new BufferedImage(scaledWidth, scaledHeight, image.getType());
+                Graphics2D graphics = scaledImage.createGraphics();
+                graphics.drawImage(image, 0, 0, scaledWidth, scaledHeight, null);
+                graphics.dispose();
+                image = scaledImage;
+                System.out.println("Image resized to fit screen dimensions");
+            }
+
             // Initialize display
             JLabel label = new JLabel(new ImageIcon(image));
             JFrame frame = src.utils.DisplayUtils.createMainFrame(label);
             frame.setVisible(true);
 
             // Process image
+            System.out.println("Processing in " + (mode.equals("S") ? "single" : "multi") + "-threaded mode...");
             if (mode.equals("S")) {
                 new ImageProcessor(image, label, squareSize, 0, image.getHeight()).run();
             } else if (mode.equals("M")) {
